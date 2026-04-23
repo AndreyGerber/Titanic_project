1d9c4d8e9899d85b3a5f4c69bc7b01f66d02055c
Andrey Gerber <andrey.gerber.88@gmail.com>
2026-04-23 14:00:39 +0000

    update

--- streamlit.py
+++ streamlit.py
@@ -10,3 +10,13 @@ st.title("Titanic : binary classification project")
 st.sidebar.title("Table of contents")
 pages=["Exploration", "DataVizualization", "Modelling"]
 page=st.sidebar.radio("Go to", pages)
+
+if page == pages[0] : 
+  st.write("### Presentation of data")
+
+st.dataframe(df.head(10))
+st.write(df.shape)
+st.dataframe(df.describe())
+
+if st.checkbox("Show NA") :
+  st.dataframe(df.isna().sum())
